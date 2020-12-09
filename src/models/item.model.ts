ff8db4794810440571eed6013e627121fb78c035
Abdusalam Yabrak <abdusalam.yab1996@gmail.com>
2020-12-09 11:26:16 +0100

    added imageUrl field in item.model.ts

--- src/models/item.model.ts
+++ src/models/item.model.ts
@@ -1,6 +1,7 @@
 export interface Item {
-    itemId: number;
-    sellerId: number;
-    price: number;
-    itemName: string;
+  itemId: number;
+  sellerId: number;
+  price: number;
+  itemName: string;
+  imageUrl: string;
 }
