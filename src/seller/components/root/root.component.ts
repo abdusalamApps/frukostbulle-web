1a9b9de272118feb8efdd72886f8cecb16dd9127
abdusalamyabrak <abdusalam.yab1996@gmail.com>
2020-12-19 10:52:27 +0100

    * Nav-icon animation

--- src/seller/components/root/root.component.ts
+++ src/seller/components/root/root.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RouterOutlet} from "@angular/router";
+import {animate, query, style, transition, trigger} from "@angular/animations";
+
+export const fader =
+  trigger('routerAnimations', [
+    transition('* <=> *', [
+      query(':enter, :leave', [
+        style({
+          position: 'absolute',
+          left: 0,
+          width: '100%',
+          opacity: 0,
+        })
+      ]),
+      // Animate the new page
+      query(':enter', [
+        animate('400ms ease', style({opacity: 1}))
+      ])
+    ])
+  ])
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './root.component.html',
-  styleUrls: ['./root.component.scss']
+  styleUrls: ['./root.component.scss'],
+  animations: [fader]
 })
 export class RootComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  prepareRouter(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
+
 }
+
