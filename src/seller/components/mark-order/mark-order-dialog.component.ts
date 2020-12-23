cfdd982cc5a1ded09d2a3c03e23a9ecb46b5c996
Abdusalam Yabrak <abdusalam.yab1996@gmail.com>
2020-12-23 11:38:16 +0100

    * Mark order delivered * Mark order paid * Mark order fake

--- src/seller/components/mark-order/mark-order-dialog.component.ts
+++ src/seller/components/mark-order/mark-order-dialog.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {Store} from '@ngrx/store';
+import {OrdersService} from '../../services/orders.service';
+import {OrderDetailsCardComponent} from '../order-details/order-details-card/order-details-card.component';
+
+export enum MarkType {
+  PAID,
+  DELIVERED,
+  FAKE
+}
 
 @Component({
   selector: 'app-mark-order',
@@ -7,9 +17,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarkOrderDialog implements OnInit {
 
-  constructor() { }
+
+  constructor(
+    public dialogRef: MatDialogRef<MarkOrderDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: {
+      function: any,
+      title: string
+      object?: any;
+      service?: any;
+    },
+  ) { }
 
   ngOnInit(): void {
   }
 
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onYesClick(): void {
+    this.data.function(this.data.object, this.data.service);
+    this.dialogRef.close();
+  }
 }
--- src/seller/components/order-details/order-details-card/order-details-card.component.ts
+++ src/seller/components/order-details/order-details-card/order-details-card.component.ts
@@ -1,18 +1,21 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Order} from 'src/models/order.model';
 import {Item} from '../../../../models/item.model';
-import {Observable} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 import {User} from '../../../../models/user.model';
 import {State, Store} from '@ngrx/store';
 import * as fromState from '../../../state';
+import {OrdersService} from '../../../services/orders.service';
+import {MarkOrderDialog, MarkType} from '../../mark-order/mark-order-dialog.component';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-order-details-card',
   templateUrl: './order-details-card.component.html',
   styleUrls: ['./order-details-card.component.scss']
 })
-export class OrderDetailsCardComponent implements OnInit {
-  @Input() order: Order | null = {
+export class OrderDetailsCardComponent implements OnInit, OnDestroy {
+  @Input() order: Order = {
     id: -1,
     sellerId: -1,
     buyerId: -1,
@@ -30,10 +33,13 @@ export class OrderDetailsCardComponent implements OnInit {
     content: []
   };
 
-
   buyer$ = new Observable<User | null>();
 
-  constructor(private store: Store<fromState.SellerState>) {
+  sub$ = new Subscription();
+
+  constructor(private store: Store<fromState.SellerState>,
+              private orderService: OrdersService,
+              private dialog: MatDialog) {
 
   }
 
@@ -46,6 +52,73 @@ export class OrderDetailsCardComponent implements OnInit {
 
   getOrderTotal(orderId: number): Observable<number> {
     return this.store.select(fromState.getOrderTotal, {orderId});
+  }
+
+  private onMarkClick(markType: MarkType) {
+    switch (markType) {
+      case MarkType.PAID:
+        this.dialog.open(MarkOrderDialog, {
+          data: {
+            function: this.onMarkPaid,
+            title: 'Markera betald?',
+            object: this.order,
+            service: this.orderService
+          }
+        });
+        break;
+      case MarkType.DELIVERED:
+        this.dialog.open(MarkOrderDialog, {
+          data: {
+            function: this.onMarkDelivered,
+            title: 'Markera levererad?',
+            object: this.order,
+            service: this.orderService
+          }
+        });
+        break;
+      case MarkType.FAKE:
+        this.dialog.open(MarkOrderDialog, {
+          data: {
+            function: this.onMarkFake,
+            title: 'Markera fejk?',
+            object: this.order,
+            service: this.orderService
+          }
+        });
+        break;
+    }
+  }
+
+  public onMarkDelivered(order: Order, service: OrdersService): void {
+    console.log(`onMarkDelivered called`)
+    if (order) {
+      this.sub$ = service.markOrderDelivered(order.id).subscribe(
+        res => console.log(`changed ${res}`),
+        err => console.log(`error ${err}`)
+      );
+    }
+  }
+
+  public onMarkPaid(order: Order, service: OrdersService): void {
+    if (order) {
+      this.sub$ = service.markOrderPaid(order.id).subscribe(
+        res => console.log(`changed ${res}`),
+        err => console.log(`error ${err}`)
+      );
+    }
+  };
+
+  public onMarkFake(order: Order, service: OrdersService): void {
+    if (order) {
+      this.sub$ = service.markOrderFake(order.id).subscribe(
+        res => console.log(`changed ${res}`),
+        err => console.log(`error ${err}`)
+      );
+    }
+  };
+
+  ngOnDestroy(): void {
+    this.sub$.unsubscribe();
   }
 
 }
