6b429a20f9a4c6db4246355ef76e7ba31b1a6ad8
Abdusalam Yabrak <abdusalam.yab1996@gmail.com>
2020-12-23 10:43:51 +0100

    * Added service methods to mark order

--- src/seller/services/orders.service.ts
+++ src/seller/services/orders.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import * as urls from '../../urls';
 import {Observable} from 'rxjs';
@@ -9,13 +9,30 @@ import {Order} from '../../models/order.model';
 })
 export class OrdersService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   getOrdersBySellerId(sellerId: number): Observable<Order[]> {
-    return this.http.get<Order[]>(`${urls.ordersUrls.Get.getOrdersBySellerIdUrl}${sellerId}`)
+    return this.http.get<Order[]>(`${urls.ordersUrls.Get.getOrdersBySellerIdUrl}${sellerId}`);
   }
 
   getOrderHistoryBySellerId(sellerId: number): Observable<Order[]> {
-    return this.http.get<Order[]>(`${urls.ordersUrls.Get.gerOrderHistoryBySellerIdUrl}${sellerId}`)
+    return this.http.get<Order[]>(`${urls.ordersUrls.Get.gerOrderHistoryBySellerIdUrl}${sellerId}`);
+  }
+
+  updateOrder(order: Order): Observable<any> {
+    return this.http.post<any>(`${urls.ordersUrls.Post.updateOrderUrl}`, order);
+  }
+
+  markOrderDelivered(orderId: number) {
+    return this.http.post<any>(`${urls.ordersUrls.Post.markOrderDeliveredUrl}${orderId}`, {})
+  }
+
+  markOrderPaid(orderId: number) {
+    return this.http.post<any>(`${urls.ordersUrls.Post.markOrderPaidUrl}${orderId}`, {})
+  }
+
+  markOrderFake(orderId: number) {
+    return this.http.post<any>(`${urls.ordersUrls.Post.markOrderFakeUrl}${orderId}`, {})
   }
 }
