213d1d9535fcdfa8a811160bf22dae138119506b
Abdusalam Yabrak <abdusalam.yab1996@gmail.com>
2020-12-25 18:28:24 +0100

    * Disabled strict type checking in the compile options to prevent ej2 calendar from throwing object i undefined error

--- src/buyer/components/seller-details/seller-details.component.ts
+++ src/buyer/components/seller-details/seller-details.component.ts
@@ -5,6 +5,7 @@ import * as fromState from '../../state';
 import * as fromRoot from '../../../app/state';
 import {Observable, of} from 'rxjs';
 import {Bakery} from '../../../models/bakery.model';
+import {tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-seller-details',
@@ -18,17 +19,54 @@ export class SellerDetailsComponent implements OnInit {
 
   title = 'Säljarens detaljer';
 
+  sellerDates: Date[] = [];
+  public dateValue: Date = new Date();
+
   constructor(private store: Store<fromState.BuyerState>,
               private rootStore: Store<fromRoot.State>) {}
 
   ngOnInit(): void {
-/*
-    this.userObservable$ = this.store.select(fromState.getCurrentUser);
-    this.associatedSeller$ = this.store.select(fromState.getCurrentUserAssociatedSeller);
-    this.bakery$ = this.store.select(fromState.getCurrentUserAssociatedBakery);
-*/
+
+    this.userObservable$ = this.store.select(fromState.getSelectedSeller).pipe(
+      tap(user => {
+        if (user) {
+          this.sellerDates = user.availableDates;
+        }
+      })
+    );
+
+    /*    this.associatedSeller$ = this.store.select(fromState.getCurrentUserAssociatedSeller);
+        this.bakery$ = this.store.select(fromState.getCurrentUserAssociatedBakery);
+    */
 
   }
 
+  disableDate(args: any) {
+    if (!this.contains(args.date)) {
+      args.isDisabled = true;
+    }
+  }
+
+  private contains(date: Date): boolean {
+    let formattedCalendarDate = JSON.stringify(date.toJSON()).split('T')[0].substring(1)
+    for (let i = 0; i < this.sellerDates.length; i++) {
+      if (formattedCalendarDate === this.sellerDates[i].toString()) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  associateSeller(sellerId: number): void {
+    const buyerIdStorage = localStorage.getItem('currentUserId');
+    if (buyerIdStorage) {
+      console.log(`buyerId: ${buyerIdStorage}`);
+      const buyerId = parseInt(buyerIdStorage, 10);
+      this.store.dispatch(new fromState.BuyerUpdateSeller({
+        buyerId,
+        sellerId
+      }));
+    }
+  }
 
 }
