116cccfd7f2d209cd21b943e746cdfa3ab208647
Abdusalam Yabrak <abdusalam.yab1996@gmail.com>
2020-12-28 20:01:50 +0100

    (1) Fixed post requests after changes in the backend (2) Added resetPassword() and sendResetPassEmail() in users.service.ts (3) Added the necessary urls for (2)

--- src/seller/components/signup/signup.component.ts
+++ src/seller/components/signup/signup.component.ts
@@ -5,24 +5,20 @@ import {
   OnInit,
   ViewChild,
 } from '@angular/core';
-import {Location} from '@angular/common';
-import {ActivatedRoute, Router} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
 import {MatSnackBar} from '@angular/material/snack-bar';
-import {FormBuilder, FormControl, Validators} from '@angular/forms';
+import {FormControl, Validators} from '@angular/forms';
 import {MapsAPILoader} from '@agm/core';
 import {LocationService} from 'src/app/services/location.service';
 import {MatAccordion} from '@angular/material/expansion';
-import {tap} from 'rxjs/operators';
 import {Observable, Subscription} from 'rxjs';
 import {Bakery} from '../../../models/bakery.model';
 import {CitiesService} from '../../services/cities.service';
 import {Store} from '@ngrx/store';
 import {PermissionLevel, User} from '../../../models/user.model';
-import {BakeryService} from '../../services/bakery.service';
 import {UsersService} from '../../services/users.service';
 import * as fromState from '../../state';
 import * as fromRoot from 'src/app/state';
-import {AreaService} from '../../services/area.service';
 
 declare const google: any;
 
--- src/seller/services/users.service.ts
+++ src/seller/services/users.service.ts
@@ -15,7 +15,7 @@ export class UsersService {
   getUserByEmail(email: string): Observable<User> {
     const jsonString = JSON.stringify(email);
     const e = jsonString.substring(12, jsonString.search(',') - 1);
-    console.log('email in the user.service: ' + email)
+    console.log('email in the user.service: ' + email);
     return this.http.get<User>(urls.usersUrls.Get.getUserByEmailUrl + email);
 
   }
@@ -36,26 +36,36 @@ export class UsersService {
   createUser(user: User, coordinates: { lat: number, lng: number }[]): Observable<number> {
     return this.http.post<number>(`${urls.usersUrls.Post.createUserUrl}`, {user, coordinates});
   }
-  createUserNoPass(user: User, coordinates: []): Observable<number>{
+
+  createUserNoPass(user: User, coordinates: []): Observable<number> {
     return this.http.post<number>(`${urls.usersUrls.Post.createUserNoPassUrl}`, {user, coordinates});
   }
+
   sendConfirmationEmail(email: string): Observable<any> {
     return this.http.post(`${urls.usersUrls.Post.sendCreateEmailUrl}${email}`, {});
   }
 
   confirmAccount(userId: number, code: number): Observable<any> {
-    return this.http.post(`${urls.usersUrls.Post.confirmAccountUrl}?userId=${userId}&code=${code}`, {})
+    return this.http.get(`${urls.usersUrls.Post.confirmAccountUrl}?userId=${userId}&code=${code}`);
   }
 
   associateSeller(buyerId: number, sellerId: number): Observable<any> {
     return this.http.post<any>(
-      `${urls.usersUrls.Post.associateSellerUrl}?buyerId=${buyerId}&sellerId=${sellerId}`, {});
+      `${urls.usersUrls.Post.associateSellerUrl}`, {firstId: buyerId, secondId: sellerId});
   }
 
   deleteUser(userId: number): Observable<any> {
     return this.http.post<any>(
       `${urls.usersUrls.Post.deleteUserByIdUrl}${userId}`,
       {}
-    )
+    );
+  }
+
+  resetPassword(userId: number, code: number): Observable<boolean> {
+    return this.http.get<boolean>(`${urls.usersUrls.Get.resetPasswordUrl}?userId=${userId}&code=${code}`);
+  }
+
+  sendResetPassEmail(email: string): Observable<string> {
+    return this.http.post<string>(`${urls.usersUrls.Post.sendResetPassEmailUrl}?userEmail=${email}`, {});
   }
 }
